819eca98e3e13296379da3cede707466c107dd0a
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-03 22:19:35 +0300

    getting knowledge

--- types/types1.js
+++ types/types1.js
@@ -1,2 +1,20 @@
-var str = 'RuslanPr0g';
-console.log(str);
+var isFetching = true;
+var isLoading = false;
+var int = 42;
+var float = 42.2;
+var num = 3e28;
+var message = "RuslanPr0g";
+var array = [1, 2, 3, 4];
+var array2 = [2, 3, 3, 3];
+var array3 = ['1', '2', '3', '4'];
+var array4 = ['2', '3', '4', '5'];
+// Tuple
+var array5 = ['1', 4];
+// Any
+var variable = 42;
+variable = 'ss';
+// ===
+var foo = function (name) {
+    console.log(name);
+};
+foo("lol");
--- types/types1.ts
+++ types/types1.ts
@@ -1,3 +1,30 @@
-const str: string = 'RuslanPr0g';
+const isFetching: boolean = true;
+const isLoading: boolean = false;
 
-console.log(str);
+let int: number = 42; 
+let float: number = 42.2; 
+let num: number = 3e28;
+
+const message: string = "RuslanPr0g";
+
+const array: number[] = [1, 2, 3, 4];
+const array2: Array<number> = [2, 3, 3, 3];
+
+const array3: string[] = ['1', '2','3', '4'];
+const array4: Array<string> = ['2', '3', '4', '5'];
+
+// Tuple
+const array5: [string, number] = ['1', 4];
+
+// Any
+let variable: any = 42;
+
+variable = 'ss';
+
+// ===
+const foo = (name: string): void => {
+    console.log(name);
+}
+
+foo("lol");
+
