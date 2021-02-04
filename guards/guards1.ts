b750457493591376b41bc8aab0e227ba2a4b6a3f
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-04 09:17:13 +0300

    guards

---
+++ guards/guards1.ts
@@ -0,0 +1,37 @@
+function strip(x: string | number) {
+    if(typeof x === 'number')
+    {
+        return x.toFixed(2);
+    }
+
+    return x.trim();
+}
+
+class MyResponse {
+    header = 'response header';
+    result = 'response result';
+}
+
+class MyError {
+    header = 'error header';
+    result = 'error result';
+}
+
+function handle(res: MyResponse | MyError) {
+    if(res instanceof MyResponse)
+    {
+        return {
+            info: res.header + res.result
+        }
+    }
+
+    if(res instanceof MyError)
+    {
+        return {
+            info: res.header + res.result
+        }
+    }
+}
+
+// ======================
+
