fce13f753e71dc1c5a4f44ed66a55b9fa3d3e525
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-04 09:23:58 +0300

    generics

---
+++ generics/generics1.ts
@@ -0,0 +1,8 @@
+const arrayOfNumbers: Array<number> = [1, 2, 3];
+const arrayOfStrings: Array<string> = ['1', '2', '3'];
+
+function reverse<T>(array: T[]): Array<T>
+{
+    return array.reverse();
+}
+
