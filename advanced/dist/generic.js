a64c69ab184f17ba29e9180a1159aa2e5dbd1019
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-10 00:46:03 +0300

    object assign

--- advanced/dist/generic.js
+++ advanced/dist/generic.js
@@ -1,1 +1,5 @@
 "use strict";
+function mergeObjects(a, b) {
+    return Object.assign({}, a, b);
+}
+const merged = mergeObjects({ name: 'Vladian' }, { age: 26 });
--- advanced/src/generic.ts
+++ advanced/src/generic.ts
@@ -1,12 +1,18 @@
 // const cars: string[]= ['Ford', 'Audi', ''];
 // const cars2: Array<string> = ['Ford', 'Audi', ''];
 
-const promise  = new Promise<string>(resolve => {
-    setTimeout(() => {
-        resolve('Resolving....');
-    }, 2000)
-});
+// const promise  = new Promise<string>(resolve => {
+//     setTimeout(() => {
+//         resolve('Resolving....');
+//     }, 2000)
+// });
 
-promise.then(data => {
-    console.log(data);
-})
+// promise.then(data => {
+//     console.log(data);
+// })
+
+function mergeObjects(a: object, b: object) {
+    return Object.assign({}, a, b);
+}
+
+const merged = mergeObjects({name: 'Vladian'}, {age: 26});
