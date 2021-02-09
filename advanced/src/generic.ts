7b5794167d15a986c659a4794ef6f6002d464dfd
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-10 00:40:35 +0300

    promises

--- advanced/src/generic.ts
+++ advanced/src/generic.ts
@@ -1,3 +1,9 @@
 // const cars: string[]= ['Ford', 'Audi', ''];
 // const cars2: Array<string> = ['Ford', 'Audi', ''];
 
+const promise  = new Promise(resolve => {
+    setTimeout(() => {
+        resolve('Resolving....');
+    }, 2000)
+});
+
