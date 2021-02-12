f3b72f7b5a8d7beb53fdd5362dd1b05fc25e0efd
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-12 23:14:22 +0300

    generic class

--- advanced/dist/generic.js
+++ advanced/dist/generic.js
@@ -3,3 +3,29 @@ function mergeObjects(a, b) {
     return Object.assign({}, a, b);
 }
 const merged = mergeObjects({ name: 'Vladian' }, { age: 26 });
+function withCount(value) {
+    return {
+        value,
+        count: `In this object ${value.length} symbols.`
+    };
+}
+function getObjectValue(obj, key) {
+    return obj[key];
+}
+class Collection {
+    constructor(_items = []) {
+        this._items = _items;
+    }
+    add(item) {
+        this._items.push(item);
+    }
+    remove(item) {
+        this._items = this._items.filter(i => i !== item);
+    }
+    get items() {
+        return this._items;
+    }
+}
+const strings = new Collection(['I', 'Am', 'Ruslan']);
+strings.add('!');
+strings.remove('Am');
--- advanced/src/generic.ts
+++ advanced/src/generic.ts
@@ -31,3 +31,30 @@ function withCount<T extends ILength>(value: T): {value: T, count: string} {
 function getObjectValue<T extends object, R extends keyof T>(obj: T, key: R) {
     return obj[key];
 }
+
+// ==========================
+
+class Collection<T> {
+    constructor(private _items: Array<T> = []) {
+
+    }
+
+    add(item: T) {
+        this._items.push(item);
+    }
+
+    remove(item: T) {
+        this._items = this._items.filter(i => i !== item);
+    }
+
+    get items(): Array<T> {
+        return this._items;
+    }
+}
+
+const strings = new Collection(['I', 'Am', 'Ruslan']);
+strings.add('!');
+strings.remove('Am');
+
+// ======================
+
