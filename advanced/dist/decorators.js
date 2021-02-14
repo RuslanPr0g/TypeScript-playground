1580b3d7f802a8c0d0f3ea65930a792bf92bb6f2
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-14 18:24:10 +0300

    decorators for class properties

--- advanced/dist/decorators.js
+++ advanced/dist/decorators.js
@@ -1,1 +1,30 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Log(constructor) {
+}
+function Log2(target, propName) {
+}
+function Log3(target, propName, descriptor) {
+}
+let Component = class Component {
+    constructor(name) {
+        this.name = name;
+    }
+    logName() {
+        console.log('Component Name: ' + this.name);
+    }
+};
+__decorate([
+    Log2
+], Component.prototype, "name", void 0);
+__decorate([
+    Log3
+], Component.prototype, "logName", null);
+Component = __decorate([
+    Log
+], Component);
--- advanced/src/decorators.ts
+++ advanced/src/decorators.ts
@@ -1,8 +1,25 @@
-function Log() {
+function Log(constructor: Function) {
+
+}
+
+function Log2(target: any, propName: string | Symbol) {
+}
+
+function Log3(target: any, propName: string | Symbol, descriptor: PropertyDescriptor) {
 
 }
 
 @Log
 class Component {
+    @Log2   
+    name: string
 
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    @Log3
+    logName(): void {
+        console.log('Component Name: ' + this.name);
+    }
 }
